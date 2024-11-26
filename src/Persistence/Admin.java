bb9b9dc534947c6fd439846f07cd9e5feb718115
zhacode28 <pinkuvoisu@gmail.com>
2024-11-26 20:25:25 +0700

    Add files via upload

---
+++ src/Persistence/Admin.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistence;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author zahidahhanumalzahra
+ */
+@Entity
+@Table(name = "admin")
+@NamedQueries({
+    @NamedQuery(name = "Admin.findAll", query = "SELECT a FROM Admin a"),
+    @NamedQuery(name = "Admin.findByUsername", query = "SELECT a FROM Admin a WHERE a.username = :username"),
+    @NamedQuery(name = "Admin.findByKatasandi", query = "SELECT a FROM Admin a WHERE a.katasandi = :katasandi")})
+public class Admin implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Column(name = "katasandi")
+    private String katasandi;
+
+    public Admin() {
+    }
+
+    public Admin(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKatasandi() {
+        return katasandi;
+    }
+
+    public void setKatasandi(String katasandi) {
+        this.katasandi = katasandi;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Admin)) {
+            return false;
+        }
+        Admin other = (Admin) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "iya.Admin[ username=" + username + " ]";
+    }
+    
+}
